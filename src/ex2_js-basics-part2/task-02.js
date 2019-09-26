d9e676d690f96834268da227f210fcc93604c901
VsemirnovNikita <vsemir4ik@mail.ru>
2019-09-26 20:44:16 +0300

    Testing

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,9 @@
+function DisplayArrayAndFindItLenght(array){
+    let count=0;
+    for (let i=0; i<array.length;i++){
+        console.log(array[i]);
+        count++;
+    }
+    console.log(count);
+}
+module.exports=DisplayArrayAndFindItLenght;
