47963304b89b13769fbf052b946988f932de9583
VsemirnovNikita <vsemir4ik@mail.ru>
2019-10-02 14:20:48 +0300

    deleted empty lines

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,8 +1,6 @@
 function DisplayArrayAndFindItLenght(array){
-    
     for (let i=0; i<array.length;i++){
-        console.log(array[i]);
-       
+        console.log(array[i]); 
     }
     console.log(array.length);
 }
