b2e8f85af8b85ccbe67a62a781a4ae2432edd44d
VsemirnovNikita <vsemir4ik@mail.ru>
2019-10-08 13:15:09 +0300

    Find substring with less code

--- src/ex4_js-objects-part2/task-05.js
+++ src/ex4_js-objects-part2/task-05.js
@@ -1,6 +1,4 @@
 function findSubstring (substring,someString){
-    let isSubstring=false;
-    if (someString.indexOf(substring)!=-1) isSubstring=true;
-    return isSubstring;
+    return someString.includes(substring)
 }
 module.exports=findSubstring;
