1f482399ad4b2643459a9fe32b8b155815e7aa92
VsemirnovNikita <vsemir4ik@mail.ru>
2019-10-08 18:19:32 +0300

    Bug fixes in task-08

--- src/ex4_js-objects-part2/task-08.js
+++ src/ex4_js-objects-part2/task-08.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 function createLowerCamelCaseString(someString){
     if(!someString) return someString
-    someString.toLowerCase();
+    someString=someString.toLowerCase();
     for (let i=0; i<someString.length;i++){
         if (someString[i-1]===' ') someString=someString.slice(0,i-1)+someString[i].toUpperCase()+someString.slice(i+1)
     }
