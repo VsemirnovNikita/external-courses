f8ddf580274626d6d9a2f2491027c1268947cad4
VsemirnovNikita <vsemir4ik@mail.ru>
2019-10-09 19:58:41 +0300

    Change is nan check

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,9 +1,9 @@
 function chooseNumberOrString(argument){
     let argumentType;
-    if (typeof argument==='string') argumentType='string'
-    else {
-        if ((typeof argument==='number')&&(!argument.isNan)) argumentType='number';
-    }
+    if ((typeof argument==='number')||!isNaN(argument)) argumentType='number'
+    else argumentType= undefined;
+    if (typeof argument==='string') argumentType='string';
+    if (!((typeof argument==='number')||(typeof argument==='string'))) argumentType=undefined;
  return argumentType;
 }
 module.exports=chooseNumberOrString;
