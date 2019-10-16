124d369f5acf2cc758bc56915e6c9e5948e69597
VsemirnovNikita <vsemir4ik@mail.ru>
2019-10-16 18:59:13 +0300

    Add lost broken task-11

---
+++ src/ex4_js-objects-part2/task-11.js
@@ -0,0 +1,7 @@
+function countSameLetters(someString){
+    let regex=/a-z/g;
+    for (let i=0;i<someString.length;i++)
+    if (someString.match(regex))
+    console.log ()
+}
+//module.exports=countSameLetters;
