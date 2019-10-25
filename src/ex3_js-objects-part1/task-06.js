1fcfe4ca3bfebecc56fd4482b1797796ee8291a7
VsemirnovNikita <vsemir4ik@mail.ru>
2019-10-25 17:31:12 +0300

    Try wierd solution

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,4 +1,29 @@
+/* eslint-disable guard-for-in */
 function doDeepCopy(someObject){
+    
+    let copy ={},typeObject=Object.prototype.toString;
+    if(someObject!==null){
+    for (let key in someObject){
+    switch(typeObject.call(key)){
+        case 'object Object': copy[key]=doDeepCopy(someObject[key])
+            break;
+        case 'object Array': for (let i=0;i<key.length;i++){
+            if(key[i] != null && typeof key[i] === "object") copy[key[i]]=doDeepCopy(key[i]) 
+            else copy[key[i]]=someObject[key[i]]
+        }
+            break;
+        default: copy[key]=someObject[key];
+            break;
+    }
+    }
+    }
+    
+    
+    
+    
+    
+    
+    /*
     let copy = Object.assign({},someObject)
     for (let key in someObject){
         if(someObject[key] != null && typeof someObject[key] === "object"){
@@ -6,6 +31,7 @@ function doDeepCopy(someObject){
             else copy[key]=doDeepCopy(someObject[key]);  
         } 
     }
+    */
     return copy;
 }
 module.exports=doDeepCopy;
