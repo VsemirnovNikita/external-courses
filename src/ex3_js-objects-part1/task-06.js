55bb8cea1a8c3d65f5c5c60177b69c9d07305365
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-03 14:47:25 +0300

    Fix copy of array

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -9,12 +9,12 @@ function doDeepCopy(someObject){
             break;
         case '[object Array]': copy[key]=[];
             for (let i=0;i<someObject[key].length;i++){
-            if(someObject[key][i] != null && typeof someObject[key][i] === "object") copy[key[i]]=doDeepCopy(someObject[key][i]) 
+            if(someObject[key][i] != null && typeof someObject[key][i] === "object") copy[key][i]=doDeepCopy(someObject[key][i]) 
             else copy[key][i]=someObject[key][i]
         }
             break;
         default: copy[key]=someObject[key];
-  //      console.log(typeObject.call(someObject[key]));
+     console.log(typeObject.call(someObject[key]));
             break;
     }
     }
@@ -37,7 +37,7 @@ function doDeepCopy(someObject){
     return copy;
 }
 module.exports=doDeepCopy;
- /*
+
 let a={"a": 1, "b": {"c": 0}, "e": {"f": [{"g": 1}]}}
 doDeepCopy(a);
- */
+ 
