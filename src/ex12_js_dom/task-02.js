61f6a305636cd73bc6eecb4d61ff33ba4eddf790
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-15 14:57:45 +0300

    Add menu

--- src/ex12_js_dom/task-02.js
+++ src/ex12_js_dom/task-02.js
@@ -1,20 +1,41 @@
-let isOpen=false;
-let buttonUser =document.getElementById('user');
-let arrow=document.getElementById('arrow')
-buttonUser.addEventListener( 'click',function(){
-    if(isOpen===false){
-        arrow.classList.remove('animationClose');
-        arrow.classList.add('animationOpen');
-        isOpen=true;
-        // TODO ?
+const userButton =document.getElementById('user');
+const arrowIcon=document.getElementById('arrow');
+let isMenuOpen=false;
+
+menuCreate();
+userButton.addEventListener( 'click',function(){
+    let downDrop =document.querySelector('.dropDownContent');
+    if(isMenuOpen===false){
+        arrowIcon.classList.remove('animationClose');
+        arrowIcon.classList.add('animationOpen');
+        downDrop.style.display='block';
+        isMenuOpen=true;
+        
     }
     else{
-        arrow.classList.remove('animationOpen');
-        arrow.classList.add('animationClose');
-        isOpen=false;
+        arrowIcon.classList.remove('animationOpen');
+        arrowIcon.classList.add('animationClose');
+        downDrop.style.display='none';
+        isMenuOpen=false;
     }
 })
 
-// buttonUser.addEventListener('click',function(){
-//     buttonUser.innerHTML()
-// })
+
+function menuCreate (){
+    const menu=document.createElement("div");
+    menu.classList.add('dropDownContent');
+    let menuItem=document.createElement("div");
+    menuItem.innerText='My account';
+    menuItem.classList.add('dropDownItem');
+    menu.appendChild(menuItem);
+    menuItem=document.createElement("div");
+    menuItem.innerText='My tasks';
+    menuItem.classList.add('dropDownItem');
+    menu.appendChild(menuItem);
+    menuItem=document.createElement("div");
+    menuItem.innerText='Log out';
+    menuItem.classList.add('dropDownItem');
+    menu.appendChild(menuItem);
+    
+    userButton.appendChild(menu);
+}
