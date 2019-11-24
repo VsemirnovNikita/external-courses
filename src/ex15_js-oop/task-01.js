7f15b384ac82dd74b40464c6f9b247476dd69ac8
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-24 16:53:34 +0300

    Add presents by classes

---
+++ src/ex15_js-oop/task-01.js
@@ -0,0 +1,52 @@
+class Yummy {
+    constructor(name,weight){
+        this.name=name;
+        this.weight=weight;
+    }
+    showInformation() {
+        console.log(this.name,this.weight);      
+    }    
+} 
+class Sweet extends Yummy{
+    constructor(name,weight,sugarCount){
+        super(name);
+        super(weight);
+        this.sugarCount=sugarCount;
+    }
+    showInformation() {
+        super.showInformation();
+        console.log(this.sugarCount);
+    }
+}
+class Cake extends Yummy{
+    constructor(name,weight,filling){
+        super(name);
+        super(weight);
+        this.filling=filling;
+    }
+    showInformation() {
+        super.showInformation();
+        console.log(this.filling);
+    }
+}
+class Present{
+    constructor(){
+        this.content=[new Sweet('jelly sweet',30,10),new Sweet('chocolate sweet',50,30),new Cake('gingerbread',500,'chocolate')];
+    }
+    countSugar(){
+        let countSugar;
+        for(let i =0;i<this.content.length;i++){
+            if(this.content[i] instanceof Sweet){
+                countSugar+=this.content[i].sugarCount;
+            }
+        }
+        console.log(countSugar);
+    }
+    findContent(name){
+        for(let i =0;i<this.content.length;i++){
+            if (this.content[i].name===name){
+                this.content[i].showInformation();
+            }
+        }
+    }
+}
