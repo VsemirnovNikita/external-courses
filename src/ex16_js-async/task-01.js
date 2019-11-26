13d52adb713beaec6a54ad0bafde6e0a4e04d824
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-26 15:43:13 +0300

    Add error handler

--- src/ex16_js-async/task-01.js
+++ src/ex16_js-async/task-01.js
@@ -1,9 +1,14 @@
 function myFetch(url,parametr){
     return new Promise((resolve, reject) => {
-        let xhr= new XMLHttpRequest();
+        let ok = true;
+        let xhr = new XMLHttpRequest();
         xhr.open(parametr.method,url);
         xhr.send();
-        if(xhr.status){
+        if(xhr.status>206){
+            ok=false;
+            reject("error");
+        }
+        if(ok){
             resolve(JSON.parse(xhr.responseText)); 
         }
         xhr.ontimeout = function(){
