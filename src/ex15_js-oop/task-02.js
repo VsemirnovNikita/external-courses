b0e0310923ed30440e88b4e7a39553d20b4adcc5
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-27 12:45:31 +0300

    Some fixes

--- src/ex15_js-oop/task-02.js
+++ src/ex15_js-oop/task-02.js
@@ -1,32 +1,37 @@
-function ElectricalAppliance(power,name) {
-    this.power=power;
-    this.activate=false;
-    this.name=name;
-} 
-ElectricalAppliance.prototype.activating=function (state) {
-    if(state==='on'){
-        ElectricalAppliance.activate=true;
+function ElectricalAppliance(power, name) {
+    this.power = power;
+    this.activate = false;
+    this.name = name;
+}
+ElectricalAppliance.activating = function (state) {
+    if (state === 'on') {
+        ElectricalAppliance.activate = true;
     }
-    if(state==='off'){
-        ElectricalAppliance.activate=false;
+    if (state === 'off') {
+        ElectricalAppliance.activate = false;
     }
 }
-function HeatingAppliance(temperature){
-    this.temperature=temperature;
+function HeatingAppliance(temperature, power, name) {
+    ElectricalAppliance.call(this, power, name);
+    this.temperature = temperature;
 }
-Object.setPrototypeOf(HeatingAppliance,ElectricalAppliance);
+Object.setPrototypeOf(HeatingAppliance, ElectricalAppliance);
 
-function ApplianceWithScreen(width) {
-    this.width=width
+function ApplianceWithScreen(width, power, name) {
+    ElectricalAppliance.call(this, power, name);
+    this.width = width
 }
-Object.setPrototypeOf(ApplianceWithScreen,ElectricalAppliance);
+Object.setPrototypeOf(ApplianceWithScreen, ElectricalAppliance);
 function Room(countOfSockets) {
-    this.appliances=[new HeatingAppliance('100'),new ApplianceWithScreen(300)]
+    this.appliances = [new HeatingAppliance('100', 50, 'teapot'), new ApplianceWithScreen(300, 1000, 'laptop')];
     appliances[0].activating('on');
 }
-Room.countPower=function () {
+Room.countPower = function () {
     let power;
-    for( let i=0; i< appliances.length;i++){
-        power+=appliances[i].power;
+    for (let i = 0; i < appliances.length; i++) {
+        power += appliances[i].power;
     }
+    console.log(power);
 }
+let room = new Room(12);
+Room.countPower();
