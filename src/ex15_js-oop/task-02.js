7b843a2fd373e53fee0984ea4f1a4a92eb2e7317
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-27 15:57:52 +0300

    Finish task-02

--- src/ex15_js-oop/task-02.js
+++ src/ex15_js-oop/task-02.js
@@ -2,13 +2,13 @@ function ElectricalAppliance(power, name) {
     this.power = power;
     this.activate = false;
     this.name = name;
-}
-ElectricalAppliance.activating = function (state) {
-    if (state === 'on') {
-        ElectricalAppliance.activate = true;
-    }
-    if (state === 'off') {
-        ElectricalAppliance.activate = false;
+    this.activating = function (state) {
+        if (state === 'on') {
+            this.activate = true;
+        }
+        if (state === 'off') {
+            this.activate = false;
+        }
     }
 }
 function HeatingAppliance(temperature, power, name) {
@@ -19,19 +19,19 @@ Object.setPrototypeOf(HeatingAppliance, ElectricalAppliance);
 
 function ApplianceWithScreen(width, power, name) {
     ElectricalAppliance.call(this, power, name);
-    this.width = width
+    this.width = width;
 }
 Object.setPrototypeOf(ApplianceWithScreen, ElectricalAppliance);
 function Room(countOfSockets) {
+    this.countOfSockets = countOfSockets;
     this.appliances = [new HeatingAppliance('100', 50, 'teapot'), new ApplianceWithScreen(300, 1000, 'laptop')];
-    appliances[0].activating('on');
 }
-Room.countPower = function () {
-    let power;
-    for (let i = 0; i < appliances.length; i++) {
-        power += appliances[i].power;
+Room.countPower = function (room) {
+    let power = 0;
+    for (let i = 0; i < room.appliances.length; i++) {
+        if (appliances[i].activate === true) {
+            power += room.appliances[i].power;
+        }
     }
     console.log(power);
 }
-let room = new Room(12);
-Room.countPower();
