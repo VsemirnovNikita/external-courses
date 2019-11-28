6ee1d0519122887cfe176bc99fe083e1fdf87e3b
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-28 13:30:48 +0300

    Fix comments

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,14 +1,15 @@
 const imagesArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpeg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg',]
-let image = document.getElementById('image')
+let image = document.querySelector('.image');
 function findImage(imageURL, option) {
-    let newImage = imageURL; debugger
+    let newImage = imageURL;
     if (option === "right") {
         for (let i = 0; i < imagesArray.length; i++) {
             if (imagesArray[i] === imageURL) {
                 if (i === 0) {
                     newImage = imagesArray[imagesArray.length - 1];
+                } else {
+                    newImage = imagesArray[i - 1];
                 }
-                else newImage = imagesArray[i - 1];
             }
         }
     }
@@ -18,7 +19,9 @@ function findImage(imageURL, option) {
                 if (i === imagesArray.length - 1) {
                     newImage = imagesArray[0];
                 }
-                else newImage = imagesArray[i + 1];
+                else{
+                    newImage = imagesArray[i + 1];
+                } 
             }
         }
     }
