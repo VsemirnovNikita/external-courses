a119c5569b57b9dbd0bfb23d8c2a9f1a8852c16a
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-28 17:29:29 +0300

    Fix other comments

--- src/ex12_js_dom/task-02.js
+++ src/ex12_js_dom/task-02.js
@@ -1,41 +1,34 @@
-const userButton =document.querySelector('.user');
-const arrowIcon=document.querySelector('.arrow');
-let isMenuOpen=false;
+const userButton = document.querySelector('.user');
+const arrowIcon = document.querySelector('.arrow');
+let isMenuOpen = false;
 
-menuCreate();
-userButton.addEventListener( 'click',function(){
-    let downDrop =document.querySelector('.dropDownContent');
-    if(isMenuOpen===false){
+createMenu();
+userButton.addEventListener('click', function () {
+    let downDrop = document.querySelector('.dropDownContent');
+    if (isMenuOpen === false) {
         arrowIcon.classList.remove('animationClose');
         arrowIcon.classList.add('animationOpen');
-        downDrop.style.display='block';
-        isMenuOpen=true;
-        
+        downDrop.style.display = 'block';
+        isMenuOpen = true;
     }
-    else{
+    else {
         arrowIcon.classList.remove('animationOpen');
         arrowIcon.classList.add('animationClose');
-        downDrop.style.display='none';
-        isMenuOpen=false;
+        downDrop.style.display = 'none';
+        isMenuOpen = false;
     }
 })
 
-
-function menuCreate (){
-    const menu=document.createElement("div");
+function createMenu() {
+    const menu = document.createElement("div");
+    const menuItemCaptions = ['My tasks', 'My account', 'Log out'];
+    let menuItem;   
     menu.classList.add('dropDownContent');
-    let menuItem=document.createElement("div");
-    menuItem.innerText='My account';
-    menuItem.classList.add('dropDownItem');
-    menu.appendChild(menuItem);
-    menuItem=document.createElement("div");
-    menuItem.innerText='My tasks';
-    menuItem.classList.add('dropDownItem');
-    menu.appendChild(menuItem);
-    menuItem=document.createElement("div");
-    menuItem.innerText='Log out';
-    menuItem.classList.add('dropDownItem');
-    menu.appendChild(menuItem);
-    
+    let menuItems = menuItemCaptions.map(caption => {
+        menuItem = document.createElement("div");
+        menuItem.innerText = caption;
+        menuItem.classList.add('dropDownItem');
+        menu.appendChild(menuItem);
+    })
     userButton.appendChild(menu);
 }
