d63ba50b5c1ddef3a678fc8708bddc4d5b629923
VsemirnovNikita <vsemir4ik@mail.ru>
2019-11-29 19:24:17 +0300

    Deleted extra variable

--- src/ex16_js-async/task-01.js
+++ src/ex16_js-async/task-01.js
@@ -1,18 +1,16 @@
 function myFetch(url,parametr){
     return new Promise((resolve, reject) => {
-        let ok = true;
         let xhr = new XMLHttpRequest();
         xhr.open(parametr.method,url);
         xhr.send();
-        if(xhr.status>206){
-            ok=false;
-            reject("error");
-        }
-        if(ok){
-            resolve(JSON.parse(xhr.responseText)); 
-        }
         xhr.ontimeout = function(){
             reject("failed");
         }
+        if(xhr.status!==200){
+            reject(xhr.response);
+        }
+        else{
+            resolve(JSON.parse(xhr.responseText)); 
+        }    
     })
 }  
